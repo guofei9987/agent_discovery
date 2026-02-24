832dc6f90e0c83d8fb63fca324c20e617c337c5e
guofei9987 <guofei9987@foxmail.com>
2026-02-24 13:09:41 +0800

    fetcher: concurrent

--- agent_discovery/fetcher/__init__.py
+++ agent_discovery/fetcher/__init__.py
@@ -1,23 +1,21 @@
+from concurrent.futures import ThreadPoolExecutor
 from .news_fetcher import NewsFetcher
 from .rss_fetcher import RSSFetcher
 from .arxiv_fetcher import ArxivFetcher
 
 
 def fetch_all_msg(cfg):
-    all_msg = list()
+    """并行获取所有数据源的消息"""
+    executors = []
+
     if 'news_source' in cfg:
-        fetcher1 = NewsFetcher()
-        news_msg = fetcher1.fetch_all(cfg)
-        all_msg.extend(news_msg)
-
+        executors.append(lambda: NewsFetcher().fetch_all(cfg))
     if 'rss' in cfg:
-        fetcher2 = RSSFetcher()
-        rss_msg = fetcher2.fetch_all_rss(cfg)
-        all_msg.extend(rss_msg)
-
+        executors.append(lambda: RSSFetcher().fetch_all_rss(cfg))
     if 'arXiv' in cfg:
-        arxiv = ArxivFetcher()
-        arxiv_msg = arxiv.fetch_all(cfg)
-        all_msg.extend(arxiv_msg)
+        executors.append(lambda: ArxivFetcher().fetch_all(cfg))
 
-    return all_msg
+    with ThreadPoolExecutor() as pool:
+        results = list(pool.map(lambda f: f(), executors))
+
+    return [item for r in results for item in r]
